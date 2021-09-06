8c689924cad39e3cad2adaef73ab0f138f5df3af
Normandes Junior <normandesjr@gmail.com>
2021-09-06 09:38:14 -0300

    Initial commit

---
+++ lib/starwars-serverless-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class StarwarsServerlessStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
