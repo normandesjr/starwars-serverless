f5a65cc4da943b3e658293646d3d799474824cfb
normandesjr <normandesjr@gmail.com>
2021-09-10 18:03:18 -0300

    feat: converting event to object

--- src/save-character/save-character-function.ts
+++ src/save-character/save-character-function.ts
@@ -1,9 +1,21 @@
 
+interface FullCharacter {
+  user: string,
+  character: string,
+  height: string,
+  gender: string,
+  swapi_id: number
+}
+
 export const main = async(event: any) => {
 
-  console.log(event)
+  const fullCharacter:FullCharacter = { 
+    user: event.detail.user,  
+    character: event.detail.character,
+    height: event.detail.height,
+    gender: event.detail.gender,
+    swapi_id: event.detail.swapi_id
+  }
 
-  console.log(typeof event)
-
-
+  return fullCharacter
 }
